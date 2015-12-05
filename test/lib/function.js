1bd1e508e5d77acd8fc45659f52504a138eec152
murmur76 <celica212@gmail.com>
2015-12-05 15:38:47 +0900

    Add generatorToCallback function to lib/function module.
    It receives a generator function and returns callback function that
    receives express request / response object and next callback.
    
    If the result of generator function successfully resolved, that result is
    directly sent by calling express res.send() method.
    
    If there's an error, it catches the error and call next function as its
    first argument.

---
+++ test/lib/function.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const _ = require('lodash');
+const assert = require('assert');
+const Fun = require('../../lib/function');
+
+describe('function', () => {
+  let req = {
+    body: { firstName: 'First', lastName: 'Last' },
+    params: { userId: 1 },
+    query: { age: 21 }
+  };
+  let res = { send: _.identity };
+  let next = () => {};
+
+  describe('#generatorToCallback', () => {
+    it('should convert generator function to callback function', (done) => {
+      let generatorFunc = function* (params) {
+        return yield Promise.resolve(params);
+      };
+      Fun.generatorToCallback(generatorFunc)(req, res, next).then((result) => {
+        assert.deepEqual(result, _.extend(_.clone(req.body), req.params, req.query));
+        done();
+      });
+    });
+
+    it('should encapsulate express request object into context', (done) => {
+      let generatorFunc = function* () {
+        return yield Promise.resolve(this.req);
+      };
+      Fun.generatorToCallback(generatorFunc)(req, res, next).then((result) => {
+        assert.deepEqual(result, req);
+        done();
+      });
+    });
+
+    it('should encapsulate express response object into context', (done) => {
+      let generatorFunc = function* () {
+        return yield Promise.resolve(this.res);
+      };
+      Fun.generatorToCallback(generatorFunc)(req, res, next).then((result) => {
+        assert.deepEqual(result, res);
+        done();
+      });
+    });
+
+    it('should catch error in generator function', (done) => {
+      let generatorFunc = function* () {
+        return yield Promise.reject('Error');
+      };
+      Fun.generatorToCallback(generatorFunc)(req, res, _.identity).then((result) => {
+        assert.equal(result, 'Error');
+        done();
+      });
+    });
+  });
+});
